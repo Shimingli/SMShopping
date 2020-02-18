abd7e04e1425e2786e5ecfe52b1292bfefa02d86
Administrator <737141437@qq.com>
2020-02-18 21:48:08 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # SMShopping
+小程序第三方的框架：
+1、腾讯 wepy 类似vue  
+2、美团 mpvue 类似vue
+3、京东 taro 类似react 
+4、滴滴  chameleon 变蛇龙
+5、uni-app 类似vue
+
+* 接口文档 ：https://www.showdoc.cc/128719739414963?page_id=2513235043485226
+
+* 远程字体文本 ：http://at.alicdn.com/t/font_1199223_3th2jwrfikp.css  
+
+* 阿里巴巴字体库 ：https://www.iconfont.cn/  使用github登陆
+
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,62 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+//Page Object
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    
+  //  轮播图数组
+  swiperList:[],
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  //网络请求
+  onLoad: function(options) {
+    var reqTask = wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      data: {},
+      header: {'content-type':'application/json'},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result)=>{
+         console.log(result)
+         this.setData({
+          swiperList:result.data.message
+         })
+
+
+      },
+      fail: ()=>{},
+      //成功和失败都能走
+      complete: ()=>{}
+    }); 
+
+
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  onReady: function() {
+    
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  onShow: function() {
+    
+  },
+  onHide: function() {
+
+  },
+  onUnload: function() {
+
+  },
+  onPullDownRefresh: function() {
+
+  },
+  onReachBottom: function() {
+
+  },
+  onShareAppMessage: function() {
+
+  },
+  onPageScroll: function() {
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item) {
+
   }
-})
+});
+  
